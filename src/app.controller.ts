81cf19d732011d75cf6136e79061c391db8386a3
Cristhian Villca Mamani <cristhian.villca@tugerente.com>
2024-05-21 17:46:27 -0400

    añadiendo y mejorando los comentarios del controlador principal para documentar el curso.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,55 +5,76 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  /* RUTAS SIN PARAMETROS ----------------------------------------------------- */
+  /**
+   * RUTAS SIN PARAMETROS ----------------------------
+   * a continuacion tenemos la siguientes rutas que no 
+   * necesitan parametros para redireccionar la ruta.
+   * 
+   * ejemplo: /home/caterories/lacteos
+   */
 
-  // ruta por defecto para la ruta /
+  // ruta por defecto para la ruta que devuelve un string 
   @Get()
   getHome(): string {
     return "<h1>Hola Cristhian</h1>";
   }
 
-  // ruta contacto sin usar "/"
+  // ruta contacto sin usar "/" para redireccionar a la pagina contact
   @Get('contact')
   getContact(): string {
     return "<h1>Bievenido a mi contacto</h1>"
   }
 
-  // ruta con el "/" para la ruta 
+  // ruta con el "/" para la ruta para demostrar que es opcional el "/"
   @Get('/about')
   getAbout():string {
     return "<h1>Bienvenido a sobre mi</h1>"
   }
 
-  /* RUTAS CON PARAMETROS ---------------------------------------------------------- */
+  /**
+   * RUTAS CON PARAMETROS ----------------------------
+   * a continuacion veremos las rutas para el controaldor 
+   * que si necesitan de parametros enviados juntamente en la ruta
+   * o dentro de la misma url, asi como uno y varias rutas.
+   */
   
-  // rutas con parametros usando el objeto params.
+  // rutas con parametros usando el objeto params o tambien puede ser nombrado param, 
+  // y tener varios parametros almacenados dentro de objeto param.
   @Get('product/:productId')
   getProduct(@Param() params: any):string {
     return `<h1>El producto es ${params.productId}</h1>`;
   }
 
-  // rutas con parametos especificando el parametro a usar.
+  // rutas con parametos especificando el parametro a usar, necesario para ser mas directo.
   @Get('products/:productId')
   getProducts(@Param('productId') productId: string):string {
     return `<h1>El producto es... ${productId}</h1>`;
   }
 
-  // rutas con varios parametros
+  // rutas con varios parametros especificados para luego usar los mismos.
   @Get('categories/:id/products/:productId')
   getCategory(@Param('productId') productId: string, @Param('id') id:string):string {
     return `<h1>La categoria es... ${id}, y el producto es... ${productId}</h1>`;
   }
 
 
-  /* PARAMETOS TIPO QUERY ---------------------------------------------------------- */
+  /**
+   * RUTAS CON PARAMETROS DE TIPO QUERY ----------------------------
+   * para este ejemplo tenemos parametros que son enviados en la misma URL
+   * y que tambien nos sirven para obtener los parametros para trabajar.   
+   * 
+   * ejemplo: /home/caterories?lacteos&free
+   */
 
+  //ruta services con parameotros enviados en la URL como ser limit y offset 
+  // especificados de manera general.
   @Get('services')
   getServices(@Query() params: any){
     const { limit, offset } = params;
     return  `services: limit=> ${limit} offset => ${offset}`
   }
 
+  // la misma ruta solo que con la especificacion de las rutas.
   @Get('service')
   getService(@Query('limit') limit = 200, @Query('offset') offset: string){
     return  `service es: limit=> ${limit} offset => ${offset}`
